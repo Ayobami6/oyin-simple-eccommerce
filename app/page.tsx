98eac2a85e20a442f9db00d048198f39aefeb6c3
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-08-08 10:09:31 +0100

    Add minimal category implementation

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,25 @@ import Header from "@/components/Header";
 import CarouselPlugin from "@/components/CarouselPlugin";
 
 export default function Home() {
+  const categories = ["Men's Clothing", "Women's Clothing", "Slides", "Bags"];
+  const catFilter = () => {
+    // Filter logic goes here
+    console.log("Filtering")
+  }
   return (
     <div>
       <Banner />
       <Header />
+      <div className="flex justify-center font-Inter text-sm gap-5 p-4">
+        {
+          categories.map((cat, index) => (
+            <h2 key={index} className="cursor-pointer hover:underline hover:text-submain" onClick={catFilter}>
+              {cat}
+
+            </h2>
+          ))
+        }
+      </div>
       <CarouselPlugin />
     </div>
   );
