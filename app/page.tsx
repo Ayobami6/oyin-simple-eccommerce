5287a150af0fdede691e4bb00acdb69baba519c6
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-08-12 17:27:08 +0100

    Implement the search input field

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import CarouselPlugin from "@/components/CarouselPlugin";
 import PopularProduct from "@/components/PopularProduct";
 import Footer from "@/components/Footer";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
   const categories = ["Men's Clothing", "Women's Clothing", "Slides", "Bags"];
@@ -13,10 +14,20 @@ export default function Home() {
     // Filter logic goes here
     console.log("Filtering")
   }
+  const [searchText, setSearchText] = useState("");
+  const handleSearch = () => {
+    console.log(searchText);
+    // Search logic goes here
+    console.log("Searching")
+  }
   return (
     <div>
       <Banner />
-      <Header />
+      <Header
+        searchText={searchText}
+        setSearchText={setSearchText}
+        handlePress={handleSearch}
+      />
       <div className="flex justify-center font-Inter text-sm gap-5 p-4">
         {
           categories.map((cat, index) => (
@@ -34,7 +45,7 @@ export default function Home() {
 
       </div>
       <div className="my-8">
-        <h1 className="text-center font-bold text-3xl my-10">Need Something Like This For An Affordable Price?</h1>
+        <h1 className="text-center font-bold text-2xl my-10">Need Something Like This For An Affordable Price?</h1>
         <div className="flex justify-center">
           <a href="mailto:ayobamidele006@gmail.com">
             <Button className="w-[200px] font-Poppins h-[60px] shadow-md text-[16px] text-white bg-main" type="button">
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import Image from 'next/image'
+import { FaSearch } from "react-icons/fa";
 
-type Props = {}
+type Props = {
+    searchText: string;
+    setSearchText: (event: any) => void;
+    handlePress: (event: any) => void;
+}
 
-const Header = (props: Props) => {
+const Header = ({ searchText, setSearchText, handlePress }: Props) => {
     return (
         <div className='bg-white h-[100px] flex p-10 items-center border-b-2 rounded-md'>
             <div className='flex justify-start items-center gap-5'>
                 <div>
                     <Image src={'/images/oyin.jpg'} alt='logo' width={80} height={60} />
                 </div>
-                <div>
-                    <h1>Search</h1>
+                <div className='relative'>
+                    <input
+                        type="text" className='p-2 border-2 rounded-md w-[400px] pl-10'
+                        placeholder='You looking for something?'
+                        onChange={(e) => setSearchText(e.target.value)}
+                        onKeyDown={handlePress}
+                    />
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                        <FaSearch />
+                    </div>
+
                 </div>
 
 
