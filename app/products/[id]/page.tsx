335c43a3be9a8302c3838f08913f1d900d0109bc
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-08-16 15:02:07 +0100

    Implement product detail page with dynamic Image change

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,13 +1,77 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation';
+import Banner from '@/components/Banner';
+import { fetchBanner } from '@/api/banner';
+import { fetchProductById } from '@/api/products';
+import Footer from '@/components/Footer';
+import ProductInfo from '@/components/ProductInfo';
 
 type Props = {}
 
 const ProductDetail = (props: Props) => {
   const { id } = useParams()
+  const [bannerText, setBannerText] = useState("")
+  const [bannerLoading, setBannerLoading] = useState(false)
+  const [product, setProduct] = useState(undefined)
+  const [productLoading, setProductLoading] = useState(false)
+
+  const bannerFetch = async () => {
+    try {
+      setBannerLoading(true);
+      const response = await fetchBanner();
+      if (response.status === "success") {
+        setBannerLoading(false);
+        setBannerText(response?.data.text);
+      }
+
+    } catch (error) {
+      setBannerLoading(false);
+      console.log(error);
+    }
+
+  }
+  const productFetch = async () => {
+    try {
+      setProductLoading(true);
+      const response = await fetchProductById(id as string);
+      if (response.status === "success") {
+        console.log(response)
+        setProductLoading(false);
+        setProduct(response?.data);
+      }
+    } catch (error) {
+      setProductLoading(false);
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    bannerFetch();
+    productFetch();
+  }, [])
   return (
-    <div>ProductDetail with id {id}</div>
+    <>
+      <Banner bannerText={bannerText} loading={bannerLoading} />
+      <div className='flex flex-col min-h-screen'>
+        <div className='flex-grow'>
+          {
+            product ? (
+              <ProductInfo product={product} />
+            ) : (
+              <ProductInfo />
+            )
+          }
+
+
+        </div>
+        <div className='mt-auto'>
+          <Footer />
+        </div>
+
+
+
+      </div>
+    </>
   )
 }
 
---
+++ components/ProductInfo.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+
+
+type Props = {
+    product?: any
+}
+
+const ProductInfo = ({ product }: Props) => {
+    const [activeImage, setActiveImage] = useState(product.assets[0].image)
+    const handleChangeImage = (imageString: string) => {
+        setActiveImage(imageString)
+    }
+    return (
+        <>
+            {/* large screen */}
+            <div className='hidden md:block p-[80px]'>
+                <div className='grid grid-cols-2'>
+                    {/* photo sections */}
+                    <div className='flex flex-col'>
+                        <div className='w-[80%] h-[400px] rounded-lg border-2 border-gray-200'>
+                            <div className='flex items-center pt-10 px-5 justify-center'>
+                                {
+                                    product ? (
+                                        <Image src={activeImage} alt='logo' width={300} height={200} objectFit='cover' />
+                                    ) : (
+                                        <Image src={'/images/oyin.jpg'} alt='logo' width={400} height={300} objectFit='cover' />
+                                    )
+                                }
+                            </div>
+                        </div>
+
+                        <div className='my-4'>
+                            <Carousel
+                                opts={{
+                                    align: "start",
+                                }}
+                                className="w-full max-w-sm" >
+                                <CarouselContent>
+                                    {
+                                        product && (
+                                            product.assets.map((asset: any, index: number) => (
+                                                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                                                    <div className="p-1">
+                                                        <Card className={`cursor-pointer hover:border-2`} onClick={() => setActiveImage(asset.image)}>
+                                                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                                                                <Image src={asset?.image} alt='logo' width={100} height={80} objectFit='cover' />
+                                                            </CardContent>
+                                                        </Card>
+                                                    </div>
+                                                </CarouselItem>
+                                            ))
+                                        )
+                                    }
+                                </CarouselContent>
+                                {
+                                    product.assets.length > 5 && (
+                                        <>
+                                            <CarouselPrevious />
+                                            <CarouselNext />
+                                        </>
+                                    )
+
+                                }
+
+                            </Carousel>
+
+                        </div>
+
+                    </div>
+
+                    {/* details section */}
+                    <div className='flex flex-col'>
+                        <h1 className=''>
+                            Badge
+                        </h1>
+                        <h2 className=''>
+                            Title
+                        </h2>
+
+                    </div>
+
+                </div>
+
+            </div>
+            {/* mobile screen */}
+        </>
+    )
+}
+
+export default ProductInfo
